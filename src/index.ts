f3c8353ea9b3a7e63e60da01b20eeb99c94b05a4
shirocola <kitpivatpiromgit@gmail.com>
2024-06-13 22:16:11 +0700

    add env-example and graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import routes from './routes';
 import { initDb } from './database/config';
 import setupSwagger from './swagger';
+import { Server } from 'http';
+import sequelize from './database/config';  // Use the default export for sequelize instance
 
 dotenv.config();
 
@@ -15,20 +17,61 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Initialize database and log configuration
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const sequelize = initDb();
-
-// Routes
-app.use('/api', routes);
-
-// Health check route
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'UP' });
-});
-
-setupSwagger(app);
-
-app.listen(port, () => {
+initDb().then(() => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+  console.log('Database initialized');
+
+  // Routes
+  app.use('/api', routes);
+
+  // Health check route
+  app.get('/health', (req, res) => {
+    // eslint-disable-next-line no-console
+    console.log('Health check route hit');
+    res.status(200).json({ status: 'UP' });
+  });
+
+  setupSwagger(app);
+
+  let server: Server;
+
+  if (process.env.NODE_ENV !== 'test') {
+    server = app.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is running on port ${port}`);
+    });
+  }
+
+  // Graceful shutdown
+  const shutdown = async () => {
+    // eslint-disable-next-line no-console
+    console.log('Received shutdown signal, shutting down gracefully...');
+    if (server) {
+      server.close(async (err?: Error) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.error('Error closing the server:', err);
+          process.exit(1);
+        }
+
+        // Close database connection
+        try {
+          await sequelize.close();
+          // eslint-disable-next-line no-console
+          console.log('Database connection closed');
+          process.exit(0);
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error('Error closing the database connection:', error);
+          process.exit(1);
+        }
+      });
+    }
+  };
+
+  // Handle termination signals
+  process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
 });
+
+export default app;
