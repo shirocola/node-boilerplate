7504479d7f75a2d6f2616668f3e28252cfc11d05
shirocola <kitpivatpiromgit@gmail.com>
2024-06-15 15:40:03 +0700

    change orm to typeorm

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,31 +1,21 @@
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '../../entity/User';
+import { User } from './entity/User';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [User],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
 });
 
-const connectDb = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log('Database connection established');
-  } catch (error) {
-    console.error('Database connection error:', error);
-  }
-};
-
-export default connectDb;
+export default AppDataSource;
